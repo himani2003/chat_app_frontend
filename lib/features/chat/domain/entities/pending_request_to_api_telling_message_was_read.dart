d55e36a5d4675a8b8dae4265b68d20ce5c9c9486
Himani Khurana <himani2003khurana@gmail.com>
2024-12-11 15:22:35 +0530

    chat app frontend

---
+++ lib/features/chat/domain/entities/pending_request_to_api_telling_message_was_read.dart
@@ -0,0 +1,9 @@
+
+
+class PendingRequestToApiTellingMessagesWasRead {
+  final int senderUserId;
+  final String lastMessageId;
+
+  PendingRequestToApiTellingMessagesWasRead({required this.lastMessageId, required this.senderUserId});
+
+}
